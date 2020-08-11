1b15b750dc642e0b888050d143d4cefe7f5fe269
Ryan Williams <ryan.rpwilliams@gmail.com>
2020-08-11 10:48:33 -0400

    Changed socket method

--- polytracker/src/dfsan_rt/dfsan/dfsan.cpp
+++ polytracker/src/dfsan_rt/dfsan/dfsan.cpp
@@ -36,6 +36,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
 #include <algorithm>
 #include <cstdlib>
@@ -335,6 +336,13 @@ static char *dfsan_getenv(const char *name) {
   return NULL;
 }
 
+void *get_in_addr(struct sockaddr *sa) {
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
 void dfsan_parse_env() {
   // Check for path to input file
   const char *target_file = dfsan_getenv("POLYPATH");
@@ -380,6 +388,67 @@ void dfsan_parse_env() {
 
   // POLYPORT needs to be properly processed.
   if (target_port != NULL) {
+    int sockfd, numbytes;
+    char buf[1024];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo("127.0.0.1", target_port, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        exit(1);
+    }
+
+    // Loop through all the results and connect to the first we can.
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+      fprintf(stderr, "client: failed to connect\n");
+      exit(1);
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+    printf("client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo);
+
+    if ((numbytes = recv(sockfd, buf, 1024, 0)) == -1) {
+      perror("recv");
+      exit(1);
+    }
+
+    buf[numbytes] = '\0';
+    printf("client: received '%s'\n", buf);
+
+    taint_manager->createNewTargetInfo(buf, byte_start, byte_end - 1);
+    // Special tracking for standard input
+    taint_manager->createNewTargetInfo("stdin", 0, MAX_LABELS);
+    taint_manager->createNewTaintInfo("stdin", stdin);
+
+    close(sockfd);
+  }
+
+  // POLYPORT needs to be properly processed.
+  /*if (target_port != NULL) {
     // Connect to locahost port (POLYPORT).
     int sock = 0, in_packet;
     struct sockaddr_in serv_addr;
@@ -414,7 +483,7 @@ void dfsan_parse_env() {
 
     // Close sockfd.
     close(sock);
-  }
+  }*/
 
   const char *poly_output = dfsan_getenv("POLYOUTPUT");
   if (poly_output != NULL) {

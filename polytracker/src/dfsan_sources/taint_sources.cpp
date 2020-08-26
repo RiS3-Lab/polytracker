507eab53b150ff7f2186f0a37760698a28612b43
TongweiRen <tren@wpi.edu>
2020-08-26 00:33:01 -0400

    add createNewTaintInfo into dfsw_read

--- polytracker/src/dfsan_sources/taint_sources.cpp
+++ polytracker/src/dfsan_sources/taint_sources.cpp
@@ -195,6 +195,7 @@ EXT_C_FUNC ssize_t __dfsw_read(int fd, void *buff, size_t size,
   std::string name = ss.str();
 
   taint_manager->createNewTargetInfo(name, 0, size);
+  taint_manager->createNewTaintInfo(name, fd);
   // Debug output.
   std::cout << name << std::endl;
 
